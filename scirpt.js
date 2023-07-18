58985f1d1bfe7b1158c4b2c7bd04ed67d577e574
Ahmad Jihad <ahmadjihadoffical@gmail.com>
2023-07-18 14:12:15 +0300

    fixing responsive , table, contact

--- scirpt.js
+++ scirpt.js
@@ -1,13 +1,13 @@
-function toCheck() {
-  let code = document.getElementById("code").value;
-  console.log(code);
-  if (code == "fwza03") {
-    window.location.assign("31780096366.html");
-  } else if (code == "swza11") {
-    window.location.assign("42505238001.html");
-  } else if (code == "twza80") {
-    window.location.assign("67074431050.html");
-  } else {
-    window.alert("Please enter a valid code");
-  }
-}
+// function toCheck() {
+//   let code = document.getElementById("code").value;
+//   console.log(code);
+//   if (code == "fwza03") {
+//     window.location.assign("31780096366.html");
+//   } else if (code == "swza11") {
+//     window.location.assign("42505238001.html");
+//   } else if (code == "twza80") {
+//     window.location.assign("67074431050.html");
+//   } else {
+//     window.alert("Please enter a valid code");
+//   }
+// }
